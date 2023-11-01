88dcccf56be186aa0f4d874c9452ae6a99055c2d
Mohammed Ali <mohammed70011@hotmail.com>
2023-11-01 19:23:48 +1100

    modified style, adding email regex check

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-let validate = {
-  email: function () {},
-};
+// JavaScript for form validation
+
+//Email input validation
+
+function emailCheck() {
+  let emailInput = document.getElementById("email");
+  const emailRegex = new RegExp(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/); 
+
+  document.getElementById("email-error").innerHTML = emailInput.textContent;
+  
+  /*
+  if (emailInput.match(emailRegex) === null) {
+    document.getElementById("email-error").innerHTML = "That's not the string we want";
+  } else {
+    document.getElementById("email-error").innerHTML = "This is fine!";
+  }
+*/
+
+}
